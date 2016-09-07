297a6898fe2771982f99bb0910ec588d53c9b08b
Jean-Paul Goodwin <goodwinjeanpaul@yahoo.fr>
2016-09-07 08:09:07 +0000

    Created branch with a few test methods

--- index.js
+++ index.js
@@ -23,17 +23,28 @@ bot.on("message", msg => {
 	//if message begins with "!ping"
 	if (msg.content.startsWith("!ping")) {
 		//send a message to the channel the ping message was sent in.
-		bot.reply(msg, "pong !");
-
+		bot.sendMessage(msg.channel, "Pong !");
+		
 		//alert the console
 		console.log("pong-ed " + msg.author.username);
 	}
 	
+	/* If the bot is mentioned */
+	if(msg.isMentioned(bot.user)) {
+		bot.deleteMessage(msg);
+		bot.reply(msg, "Nothing to see here!");
+		console.log("Deleted " + msg.author.username);
+	}
+	
+	if (msg.author.client.hasRole(new Discord)) {
+		console.log("checked");
+	}
+	
 	//if message begins with "!bye"
 	if (msg.content.startsWith("!bye")) {
+		bot.sendMessage(msg.channel, "Pong !");
 		//alert the console
 		console.log("disconnected by " + msg.author.username);
-		
 		//logout
 		bot.logout();
 		
