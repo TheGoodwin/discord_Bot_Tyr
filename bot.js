dea3dc6d26cb3a680576919a01af834ca7a8577d
Jean-Paul Goodwin <goodwinjeanpaul@yahoo.fr>
2016-09-15 19:28:53 +0000

    Added a logout command to disconnect safely the bot

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const Configuration = require("./conf/conf.json");
 
 //const Commands = require("./bot_modules/commands.js")(bot);
 
-
+//When the bot starts
 bot.on('ready', () => {
 	//Log the connection
 	console.log('====\nConnected to server\nCurrent version : '
@@ -26,14 +26,44 @@ bot.on('ready', () => {
 });
 
 bot.on('message', message => {
+	
 	//If the message is interpreted as a command to the bot
 	if (message.content.startsWith(Configuration.command_marker)) {
+		
 		//Get the command
 		let command = message.content.substring(1,message.content.length).toLowerCase();
+		
+		//Tests the command
 		if (command.startsWith("load")) {
 			//TODO : get and load the specified module
-		} else if (command == "help") {
-			//TODO Show all the command from the loaded module
+		} else {
+			switch (command) {
+				case 'help':
+					//TODO add the help code
+					break;
+				case 'bye' :
+					//Get the main channel of the server
+					let mainChannel = bot.channels.first();
+					
+					//Build a logout message
+					let message = bot.user + " , The Hand of Justice is retiring !\n"
+					+ "It was a pleasure to serve you all !";
+					
+					//Send the logout message to the server and log it
+					mainChannel.sendMessage(message)
+					.then(message => console.log(`Sent message: ${message.content}`))
+					.catch(console.log);
+					
+					//Wait 2000ms (time for the bot to send the message) then exit node.js without an error
+					setTimeout(function(){
+						process.exit(0);
+					}, 2000);
+					
+					break;
+				default:
+					console.log("Use of command `" + command + "` not recognized");
+					break;
+			}
 		}
 	}
 });
