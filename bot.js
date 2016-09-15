4dd2dd2f63ad98ccffb32663fe0cbf6fe2ace754
Jean-Paul Goodwin <goodwinjeanpaul@yahoo.fr>
2016-09-15 08:53:43 +0000

    Removed an unused require

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const AuthDetails = require("./auth.json");
 const Properties = require("./package.json");
-var ModuleLoader = require("./libs/module_loader.js");
 
 //const Commands = require("./bot_modules/commands.js")(bot);
 
@@ -13,6 +12,7 @@ bot.on('ready', () => {
 });
 
 bot.on('message', message => {
+	
 	//TODO add commands to load modules	
 });
 
