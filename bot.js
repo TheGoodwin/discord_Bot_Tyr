0b632c05a903ee7381d95487ca749feb5f139fa0
Jean-Paul Goodwin <goodwinjeanpaul@yahoo.fr>
2016-09-15 18:23:45 +0000

    Added TODO to the loading module block

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const AuthDetails = require("./auth.json");
 const Properties = require("./package.json");
+const Configuration = require("./conf/conf.json");
 
 //const Commands = require("./bot_modules/commands.js")(bot);
 
@@ -12,7 +13,16 @@ bot.on('ready', () => {
 });
 
 bot.on('message', message => {
-	
+	//If the message is interpreted as a command to the bot
+	if (message.content.startsWith(Configuration.command_marker)) {
+		//Get the command
+		let command = message.content.substring(1,message.content.length).toLowerCase();
+		if (command.startsWith("load")) {
+			//TODO : get and load the specified module
+		} else if (command == "help") {
+			//TODO Show all the command from the loaded module
+		}
+	}
 	//TODO add commands to load modules	
 });
 
