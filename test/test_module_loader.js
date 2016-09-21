cf74046b636f33db438c51a75e4b27583f09e38d
Jean-Paul Goodwin <goodwinjeanpaul@yahoo.fr>
2016-09-21 07:38:36 +0000

    Added tests for the getModuleDescription method

---
+++ test/test_module_loader.js
@@ -0,0 +1,76 @@
+const ModuleLoader = require("./../libs/module_loader.js");
+const assert = require("assert");
+const Errors = require("./../libs/errors.js");
+
+/**
+ * Here is a skeleton for the various test methods
+ * Just copy/paste and replace with your values
+ * 
+    function testskeleton() {
+        console.log("Processing to test the TESTSKELETON method");
+        
+        let expected = "MY_EXPECTED_VALUES";
+        let result = MY_FUNCTION_TO_TEST();
+        assert.deepEqual(result,expected);
+    }
+ *    
+**/
+
+/**
+ * Tests the getModuleDescription method with a valid module name
+ **/
+function testGetModuleDescriptionMethod1() {
+    console.log("Processing to test the getModuleDescription method 1");
+    
+    let expected = "Module allowing the use of commands to the users";
+    let result = ModuleLoader.getModuleDescription("base-commands");
+    assert.deepEqual(result,expected);
+}
+
+/**
+ * Tests the getModuleDescription method with an empty module name
+ **/
+function testGetModuleDescriptionMethod2() {
+    console.log("Processing to test the getModuleDescription method 2");
+    try {
+        ModuleLoader.getModuleDescription("");
+    } catch (e) {
+        if (e instanceof Errors.ModuleNotFoundError)
+        {
+            assert(true);
+            return;
+        }
+    }
+    assert(false);
+}
+
+/**
+ * Tests the getModuleDescription method with an wrong module name
+ **/
+function testGetModuleDescriptionMethod3() {
+    console.log("Processing to test the getModuleDescription method 3");
+    try {
+        ModuleLoader.getModuleDescription("wrongmodulename");
+    } catch (e) {
+        if (e instanceof Errors.ModuleNotFoundError)
+        {
+            assert(true);
+            return;
+        }
+    }
+    assert(false);
+}
+    
+console.log("Module_Loader tests running");
+/**
+ * Add tests here
+ **/
+
+/** GetModuleDescription tests **/
+testGetModuleDescriptionMethod1();
+testGetModuleDescriptionMethod2();
+testGetModuleDescriptionMethod3();
+
+console.log("Module_Loader tests completed");
+
+process.exit(0);
