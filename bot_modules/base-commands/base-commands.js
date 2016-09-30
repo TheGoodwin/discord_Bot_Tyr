907f26dc2a7d9410ff37b66d039aedd08b362254
SoulMourne <goodwinjeanpaul@outlook.fr>
2016-09-30 12:27:14 +0000

    The commands reacts now with the command merker

--- bot_modules/base-commands/base-commands.js
+++ bot_modules/base-commands/base-commands.js
@@ -3,19 +3,29 @@
  **/
  
 const Properties = require("./../../package.json");
+const Configuration = require("./../../conf/conf.json");
+const Command = require("./../../libs/command.js").Command;
+const Commands = require("./commands.json");
 
 module.exports = function(bot) {
     console.log("Initialization of bot commands.js module");
     
     bot.on('message', message => {
 	
-	switch (message.content) {
-		case '!author':
-			message.reply("My authors are " + Properties.author);
-			break;
-		case '!ping':
-			message.reply('pong');
-			break;
+	//If the message is interpreted as a command to the bot
+	if (message.content.startsWith(Configuration.command_marker)) {
+		
+		//Get the command
+		let cmd = new Command(message.content.substring(Configuration.command_marker.length, message.content.length));
+		
+		switch (cmd.getCommandName().toLowerCase()) {
+			case 'ping':
+				message.reply('pong');
+				break;
+			case 'pong':
+				message.reply("ping");
+				break;
+		}
 	}
 });
 
