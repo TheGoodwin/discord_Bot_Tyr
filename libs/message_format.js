a7482989515432ca7e67518719cef52731a60dae
jgoodwin <goodwinjeanpaul@outlook.fr>
2017-11-15 22:15:36 +0100

    Add doc and a format command for module names

--- libs/message_format.js
+++ libs/message_format.js
@@ -1,14 +1,15 @@
 module.exports = {
 	/**
 	 * Returns a formatted string corresponfing to the display of a command definition
+	 * @param the command
 	 **/
 	formatCommand : function(command){
 	    //Begins the string formatting
 		var str = "`- ";
-		
+
 		//Get and add to the string the command name and its the description
 		str += command.name + " : " + command.description + "\n";
-		
+
 		//If there is no parameters, display it
 		if (command.parameters == "none") {
 		    str +=  "\tNo parameters"
@@ -20,11 +21,20 @@ module.exports = {
 		        str += "\n\t\t" + parameter + " : " + command.parameters[parameter];
 		    }
 		}
-		
+
 		//Ends the string formatting
 		str += "`\n";
-		
+
 		//Returns the string
 		return str;
 	}
+
+	/**
+	 * Returns a formatted string corresponfing to the display of a module name
+	 * @param the module name
+	 **/
+	 formatModuleName : function(moduleName) {
+		 var str = "**"+moduleName+"**\n"
+		 return str;
+	 }
 }
